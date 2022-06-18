3dd9f546a58e22dd6e3772a68bc3cf990af15d65
Felipe <soufelipe@hotmail.com>
2022-06-17 23:09:58 -0300

    first commit

---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace Cronometro
+{   
+    
+    public partial class Form1 : Form
+    {
+        private Stopwatch stopwatch;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            stopwatch = new Stopwatch();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            stopwatch.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            stopwatch.Stop();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            stopwatch.Reset();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        { 
+            this.label1.Text = string.Format("{0:hh\\:mm\\:ss\\:fff}", stopwatch.Elapsed);
+
+        }
+    }
+}
